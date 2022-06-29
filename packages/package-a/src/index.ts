76937c66a60dfd79c934abeb7306c64f4a5110e2
Kelvin Luck <kelvin.luck@crowdstrike.com>
2022-06-29 17:20:53 +0100

    Illustrate problem where `@ts-expect-error` doesn't cross package boundary
    In this setup we have three packages:
    
     * `package-a` - imports a class from a `.js` file from `package-b` with
       `@ts-expect-error` and exports a subclass of it
     * `package-b` - provides that `.js` file for import
     * `package-c` - imports the subclass exported from `package-a`
    
    When you try to build this app you get an error:
    
    > packages/package-a/declarations/src/index.d.ts:1:21 - error TS2307: Cannot find module 'ts-composite-error-b/foo' or its corresponding type declarations.
    >
    > 1 import { Foo } from 'ts-composite-error-b/foo';
    
    Note that the error is present because the `.d.ts` is being type checked and
    it _doesn't_ contain the `@ts-expect-error` comment.
    
    `package-a` works fine by itself but not when consumed from _another_
    package

---
+++ packages/package-a/src/index.ts
@@ -0,0 +1,6 @@
+// @ts-expect-error
+import { Foo } from 'ts-composite-error-b/foo';
+
+export class BooFoo extends Foo {
+    isBooToo = true;
+}
---
+++ packages/package-c/src/index.ts
@@ -0,0 +1,5 @@
+import { BooFoo } from 'ts-composite-error-a';
+
+let booFoo = new BooFoo();
+
+console.log(booFoo.isBooToo);
